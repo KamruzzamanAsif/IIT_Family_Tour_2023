dea65b3e24af2214638ba65549200d64ef676715
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-02-10 23:53:31 +0600

    wheel spin

--- script.js
+++ script.js
@@ -15,18 +15,15 @@ const songs = [
   const resultDiv = document.getElementById("result");
   
   spinButton.addEventListener("click", function () {
+    // Random number between 0 and 360
+    const randomDegree = Math.floor(Math.random() * 360);
+    // Rotate the wheel
+    wheel.style.transform = `rotate(${randomDegree}deg)`;
+
+    // Random song picker
     const randomIndex = Math.floor(Math.random() * songs.length);
     const randomSong = songs[randomIndex];
     resultDiv.innerHTML = `The song you got is: "${randomSong}"`;
   });
   
 
-  // wheel spin code //
-const wheel = document.getElementById("wheel");
-
-spinButton.addEventListener("click", function () {
-  // Random number between 0 and 360
-  const randomDegree = Math.floor(Math.random() * 360);
-  // Rotate the wheel
-  wheel.style.transform = `rotate(${randomDegree}deg)`;
-});
