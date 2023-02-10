bf125ae6b16531a223531134357c70f449d2725b
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-02-11 00:28:08 +0600

    rotation done

--- script.js
+++ script.js
@@ -10,20 +10,64 @@ const songs = [
     "Hello",
     "Someone Like You",
   ];
-  
+  const wheel = document.getElementById("wheel");
   const spinButton = document.getElementById("spin-button");
   const resultDiv = document.getElementById("result");
   
   spinButton.addEventListener("click", function () {
-    // Random number between 0 and 360
-    const randomDegree = Math.floor(Math.random() * 360);
     // Rotate the wheel
-    wheel.style.transform = `rotate(${randomDegree}deg)`;
+    rotateTheWheel();
 
-    // Random song picker
-    const randomIndex = Math.floor(Math.random() * songs.length);
-    const randomSong = songs[randomIndex];
-    resultDiv.innerHTML = `The song you got is: "${randomSong}"`;
+    setTimeout(() => {
+      const randomIndex = Math.floor(Math.random() * songs.length);
+      const randomSong = songs[randomIndex];
+      resultDiv.innerHTML = `The song you got is: "${randomSong}"`;
+    }, 4000);  // 4000 miliseconds = 4 seconds 
+              // after 4 seconds the result will be shown
   });
   
 
+
+
+function rotateTheWheel(){
+  // Random number between 0 and 360
+  const randomDegree = Math.floor(Math.random() * 360);
+  // Get the current rotation of the wheel
+  const currentRotation = getRotation(wheel);
+  // Calculate the rotation difference
+  const rotationDifference = randomDegree - currentRotation;
+  // Animate the rotation
+  animateRotation(wheel, rotationDifference);
+}
+
+
+function getRotation(element) {
+  // Get the current transform value as a matrix
+  const matrix = getComputedStyle(element)
+    .getPropertyValue("transform")
+    .replace(/[^0-9\-.,]/g, "")
+    .split(",");
+  // Extract the rotation angle from the matrix
+  const angle = Math.round(Math.asin(matrix[1]) * (180 / Math.PI));
+  return angle;
+}
+
+
+function animateRotation(element, rotation) {
+  // Set the initial rotation
+  let currentRotation = 0;
+  // Set the rotation step
+  const rotationStep = 5;
+  // Start the animation loop
+  const animationInterval = setInterval(function () {
+    // Increment the current rotation
+    currentRotation += rotationStep;
+    // Rotate the wheel
+    element.style.transform = `rotate(${currentRotation}deg)`;
+    // Check if the animation has completed
+    setTimeout(() => {
+      clearInterval(animationInterval);
+    }, 4000);  // 4000 miliseconds = 4 seconds will rotate
+
+  }, 20);
+}
